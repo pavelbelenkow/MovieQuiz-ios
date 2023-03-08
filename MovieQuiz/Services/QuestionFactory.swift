6c29aaa12059b4cf79c78935973d14067b376713
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2023-03-08 22:37:16 +0300

    Added accessibility identifier to alert Minor changes to AlertPresenter and QuestionFactory classes

--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-class QuestionFactory: QuestionFactoryProtocol {
+final class QuestionFactory: QuestionFactoryProtocol {
     
     private enum Compare: String, CaseIterable {
         case more = "больше"
